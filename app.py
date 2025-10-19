5afb7bccf87bf28cb9039747a0f2871ae3a8438e
Bolaji Alabi <bolaji.o.alabi@icloud.com>
2025-10-18 20:45:36 -0400

    fixing issue

--- app.py
+++ app.py
@@ -183,7 +183,8 @@ def analyze_conversation():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
     
     
 
